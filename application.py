71edb00a7287e6c6ed9efd0d5783f16ea18e7e99
Kinshu <kc007919@gmail.com>
2020-08-02 13:05:53 -0400

    add only store 100 messages

--- application.py
+++ application.py
@@ -21,11 +21,12 @@ channelMessage['General'] = deque()
 # access Shucord.
 currentRoom = "General"
 
+# Default route
 @app.route("/")
 def index():
     return render_template("login.html")
 
-
+# The main 
 @app.route("/home", methods=["GET","POST"])
 def home():
     # Only trigger if user has submit username
@@ -48,16 +49,21 @@ def home():
     # If everythings is checked, direct them to home page
     return render_template("index.html")
 
+# User is new to the Shucord, display all important information
 @socketio.on("newUser")
 def login():
     emit("displayAll", {"user": session.get("user")})
 
+# Message Handler "Server-side"
 @socketio.on('message')
 def handle_message(message):
     currentRoom = session.get("last_channel")
+    if len(channelMessage[currentRoom]) == 100:
+        channelMessage[currentRoom].pop(0)
     channelMessage[currentRoom].append(message)
     send(message, room=currentRoom, broadcast=True)
 
+# User joining room
 @socketio.on('join')
 def on_join(data):
     username = data['username']
@@ -66,6 +72,7 @@ def on_join(data):
     join_room(room)
     send(username + ' has entered the ' + room + '.', room=room)
 
+# User leaving room
 @socketio.on('leave')
 def on_leave(data):
     username = data['username']
@@ -73,23 +80,31 @@ def on_leave(data):
     leave_room(room)
     send(username + ' has left the ' + room + '.', room=room)
 
+# Add new channel/chat room
 @socketio.on('addchannel')
 def addChan(data):
+    # Grab user input
     newChannel = data['room']
 
+    # Check if channel with the same name has already created
     if newChannel in channels:
+        # Alert user that, the channel with the same name already exist 
         emit("error", {"error": "channel already exist"})
     elif newChannel == "":
+        # Only runs when user reload pages
         emit("displayChannel", {"channels": channels},broadcast=True)
     else:
+        # Channel/chat name can be created
         channels.append(newChannel)
         channelMessage[newChannel] = deque()
         emit("displayChannel", {"channels": channels}, broadcast=True)
 
+# Display all user currently logged on
 @socketio.on('userDisplay')
 def userDis():
     emit("displayUsers",{"users": user}, broadcast=True)
 
+# Log out user
 @socketio.on('logout')
 def logOut(data):
     session.clear()
@@ -99,6 +114,7 @@ def logOut(data):
         user.remove(data['user'])
     
 
+# Re-display message to broadcast to other users
 @socketio.on('redisplayMessage')
 def displayMess(data):
     tempList = []
